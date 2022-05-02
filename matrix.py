67cccd8830da58e2bb3e6ad0636c614e3efe08cf
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-05-02 12:08:07 +0530

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,21 @@
+row= eval(input("Enter rows"))
+col=eval(input("Enter col"))
+matrix=[]
+
+resultadd = 0
+
+print("enter matrix values")
+for i in range(row):
+    demo=[]
+    for j in range(col):
+        val=int(input())
+        demo.append(val)
+    matrix.append(demo)
+
+print(matrix)
+
+for i in :
+    for j in range(col):
+        if row == col:
+            reslutadd = reslutadd + 
+
