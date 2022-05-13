aeb38f91d2e10ebf44afd395af01dba84eb24373
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-05-13 23:10:12 +0530

    Add files via upload

---
+++ Class/Hybrid_Inheritance.py
@@ -0,0 +1,23 @@
+class A:
+    def fun(self):
+        print("Am in A fun")
+
+class B:
+    def gun(self):
+        print("Am in B gun")
+
+class C(A):
+    def run(self):
+        print("Am in C run")Xx dcfcv cfcf vcf vf vcf v vvvvvvvvvvvffvfvff                          c fvfcv dddvvvvvvvvvvvvvvvvvvfd vvvvvvvvvvvd                                                                                        c cccvvvvvvvvvvvvvvvvvcccccccccccccccccccccccccccccc ccccccc  c c c cccccccccccccccccccccccccccccccccccccccccccccccc                         vcccv                                              v v vffv     v           weewwe  z
+
+class D(B,C):
+    def sun(self):
+        print("Am in D sun")
+a=A()
+b.gun()
+c=C()
+a.fun()
+b=B()
+c.fun()
+d=D()
+d.gun() zipfileZA   AZ  a
---
+++ Class/heirarchical_inheritance.py
@@ -0,0 +1,17 @@
+class A:
+    def fun(self):
+        print("Am in A fun")
+
+class B:
+    def gun(self):
+        print("Am in B gun")
+
+class C(A):
+    def run(self):
+        print("Am in C run")
+
+a=A()
+a.fun()
+b=B()
+b.gun()
+c.fun()
