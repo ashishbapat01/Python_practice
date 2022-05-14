d36e0926dcf75f14d659760d2c4f4ff30cf7fe57
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-05-14 15:32:32 +0530

    Add files via upload

---
+++ Class/object_call_variable.py
@@ -0,0 +1,22 @@
+pqr = 123
+
+
+class student:
+    xyz = 3184
+    ''' This is doc part '''
+
+    def __init__(self, id=0, name="NULL", marks=0):
+        self.id = id
+        self.name = name
+        self.marks = marks
+
+    def display(self):
+        print("id={} name={} marks={}".format(self.id, self.name, self.marks))
+
+s1=student(1,"Ashish",99)
+print(s1.__dict__)
+s1.display()
+print(s1.__dict__)
+s1.pqr=1342
+print(s1.__dict__)
+del s1.pqr
