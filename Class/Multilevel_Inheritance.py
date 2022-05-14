988d3d0de42bc60f055e824cc94d6380e1e90b99
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-05-14 15:30:59 +0530

    Add files via upload

---
+++ Class/Multilevel_Inheritance.py
@@ -0,0 +1,18 @@
+class A:
+    def fun(self):
+        print("Am in A fun")
+
+class B:
+    def gun(self):
+        print("Am in B gun")
+
+class C:
+    def run(self):
+        print("Am in C run")
+
+a=A()
+a.fun()
+b=B()
+b.gun()
+c=C()
+c.run()
