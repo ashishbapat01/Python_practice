a1d5ad8e72967e82dcd68cd2265d0c8f013c8303
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-05-19 19:21:16 +0530

    Add files via upload

---
+++ Function/Factorial_number.py
@@ -0,0 +1,16 @@
+
+def fact(n):
+    if(n==0):
+        factorial=0
+    return factorial
+
+    elif(n==1):
+        factorial=1
+        return factorial
+
+    else:
+        for i in range(1, n+1)
+            factorial=factorial*1
+        return factorial
+a=eval(input("Enter the number"))
+print("The factorial of a is",fact(a))
