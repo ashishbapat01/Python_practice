5aa09b3bafd000d96810e56cd38813c8ea2eda0c
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-05-20 17:56:13 +0530

    Add files via upload

---
+++ Function/keyword_argument.py
@@ -0,0 +1,13 @@
+'''def add(name,age):
+    print("NAme=",name, " and age=", age)
+
+
+add(age=10,name="kdn")'''
+
+'''def add(a, b=0, c=0):
+    print(a+b+c)
+
+
+add(2, 3, 4)
+add(2, 3)
+add(3)'''
