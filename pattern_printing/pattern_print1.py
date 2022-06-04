dbda8a3887ef1889700b9e889cc21cb3aecb854a
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-06-04 15:00:50 +0530

    Add files via upload

---
+++ pattern_printing/pattern_print1.py
@@ -0,0 +1,10 @@
+for i in range(1,4):
+    for j in range(1,4):
+        print("{}{}".format(i,j),end=" ")
+    print()
+
+
+for i in range (1,4):
+    for j in range(1,4):
+        print ("{}{}".format(i,j),end=" ")
+    print()
