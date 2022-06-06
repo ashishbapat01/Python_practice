057dffba15d3b1e39af2f5533effcc27a6e9caa2
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-06-06 11:17:30 +0530

    Add files via upload

---
+++ pattern_printing/changing_case_of_string.py
@@ -0,0 +1,20 @@
+s='Learning Python Is Easy'
+print("\n ----------------------------------\n")
+print(s.upper())
+print(s.lower())
+print(s.swapcase())
+print(s.title())
+print(s.capitalize())
+
+print(" \n----------------------------------\n")
+print(s.startswith("Learn"))
+print(s.endswith("of easy"))
+print(s.startswith("Python"))
+
+print("\n ----------------------------------\n")
+print(s.isalnum())
+print("KDN".isalpha())
+print("KDN".isdigit())
+print("123".isdigit())
+print('abc123'.islower())
+
