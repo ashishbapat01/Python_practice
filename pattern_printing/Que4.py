5ab6607abfef8613a6dc1ab9bc0fe46b55f32202
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-06-08 16:02:07 +0530

    Add files via upload

---
+++ pattern_printing/Que4.py
@@ -0,0 +1,11 @@
+for i in range(1,5+1):
+    for j in range(1,5+1):
+        if(i>=j):
+            print("*",end=" ")
+    print()
+
+for i in range(1,5+1):
+    for j in range(1,4+1):
+        if(i<=j):
+            print("*",end=" ")
+    print()
