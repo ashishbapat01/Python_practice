a7fbaeb06f9a383d4d295248f06bbdd9dec98373
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-06-08 16:02:53 +0530

    Add files via upload

---
+++ pattern_printing/Que2.py
@@ -0,0 +1,5 @@
+celcius=eval(input("Enter the Temprature in celcius:- "))
+print(celcius, "degree celcius",((celcius*(9/5))+32),"In Fahrenheit")
+
+far=eval(input("Enter The Teamprature in Fahrenheit: "))
+print("degree Fahrenheit ",((far-32)*(5/9)),"In celcius")
