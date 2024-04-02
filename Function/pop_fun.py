3df5f6da03930dfdd65d3326040b66603225dc60
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2024-04-02 20:13:35 +0530

    Create pop_fun.py

---
+++ Function/pop_fun.py
@@ -0,0 +1,4 @@
+my_list = [1, 2, 3, 4, 5]
+element = my_list.pop(2)
+print("Removed element:", element)  # Output: Removed element: 3
+print("Modified list:", my_list)    # Output: Modified list: [1, 2, 4, 5]
