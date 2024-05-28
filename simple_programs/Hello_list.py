e9ed7b1e99d985574a60b8755c1cf61d6ba666d0
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2024-05-28 23:31:31 +0530

    Create Hello_list.py

---
+++ simple_programs/Hello_list.py
@@ -0,0 +1,11 @@
+# Get user's name
+name = input("Enter your name: ")
+
+# Create an empty list
+name_list = []
+
+# Append the user's name to the list
+name_list.append(name)
+
+# Print the list containing the name
+print("Hello", name_list) 
